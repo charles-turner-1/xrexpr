1c242c6efb8d460b35771bb02cdf84a013b16279
Charles Turner <charles.turner@anu.edu.au>
2026-08-28 22:35:56 +0800

    Save state before I put alldims into python

--- rust/src/ir.rs
+++ rust/src/ir.rs
@@ -1,7 +1,10 @@
 use pyo3::prelude::*;
 use std::fmt::{Display, Formatter};
 
-// AllDims just represents every dimension in a dataset. Has no real fields
+/// A singleton type representing all dimensions in a dataset. 
+/// This is used to indicate that an operation should be applied to all dimensions, 
+/// rather than a specific subset of dimensions.
+/// Handy for things like `ds.mean()` which are implicitly over all dimensions.
 #[derive(Debug, Clone, Copy)]
 #[pyclass(module="xrexpr._xrexprs.ir",skip_from_py_object)]
 struct AllDims;
@@ -33,12 +36,33 @@ impl AllDims {
     }
 }
 
+/// A dimension in a dataset, represented as a string. For example, "time", "lat", "lon", etc.
+pub struct Dim(pub String);
+
+impl Display for Dim {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+ }
+
+impl From<&str> for Dim {
+    fn from(s: &str) -> Self {
+        Dim(s.to_string())
+    }
+}
+
+pub enum DimSet {
+    AllDims,
+    Concrete(std::collections::HashSet<Dim>),
+}
+
 #[pymodule]
 pub mod ir {
     use pyo3::prelude::*;
 
     #[pymodule_export]
     use super::AllDims;
+
 
     #[pymodule_init]
     fn init(m: &Bound<'_, PyModule>) -> PyResult<()> {
